efdb7b970c65e514281eed5bb6b27e3276eec2f2
李龙_win <972150074@qq.com>
2021-03-05 14:06:47 +0800

    a

--- my_context/src/main/java/com/ello/App.java
+++ my_context/src/main/java/com/ello/App.java
@@ -1,13 +1,19 @@
 package com.ello;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
 /**
- * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext run = SpringApplication.run(App.class, args);
+
+        for (String beanDefinitionName : run.getBeanDefinitionNames()) {
+            System.out.println(beanDefinitionName);
+        }
     }
 }
---
+++ my_context/src/main/java/com/ello/config/TestConfig.java
@@ -0,0 +1,10 @@
+package com.ello.config;
+
+/**
+ * @author longchenggong
+ * @Description:
+ * @date 2021/3/514:05
+ */
+
+public class TestConfig {
+}
