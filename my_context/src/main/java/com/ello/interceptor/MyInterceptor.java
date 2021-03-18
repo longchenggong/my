19e8cf21c7f23368f91f127b4a5f2f6ee9c8216a
longchenggong <longchenggong@topnet.net.cn>
2021-03-18 18:42:45 +0800

    a

---
+++ my_context/src/main/java/com/ello/interceptor/MyInterceptor.java
@@ -0,0 +1,20 @@
+package com.ello.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author longchenggong
+ * @Description:
+ * @date 2021/3/1818:03
+ */
+
+public class MyInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return false;
+    }
+}
